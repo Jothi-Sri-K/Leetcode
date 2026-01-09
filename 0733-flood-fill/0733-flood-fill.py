4eb66afd8500cd8f20d690ced3e82d145c67320a
Jothi-Sri-K <145222568+Jothi-Sri-K@users.noreply.github.com>
2026-01-09 14:33:04 +0530

    Time: 0 ms (100%), Space: 12.6 MB (79.63%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.py
@@ -0,0 +1,26 @@
+class Solution(object):
+   
+    def floodFill(self, image, sr, sc, color):
+        rows = len(image)
+        cols = len(image[0])
+        original = image[sr][sc]
+
+        if original == color:
+            return image
+
+        q = deque()
+        q.append((sr, sc))
+        image[sr][sc] = color
+
+        directions = [(1,0), (-1,0), (0,1), (0,-1)]
+
+        while q:
+            r, c = q.popleft()
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < rows and 0 <= nc < cols:
+                    if image[nr][nc] == original:
+                        image[nr][nc] = color
+                        q.append((nr, nc))
+
+        return image
